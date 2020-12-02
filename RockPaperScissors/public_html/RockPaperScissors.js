12bc9c245fa63afcc99a886a4a99eb74cc5034e6
Arthur <Arthur@Arthur-Laptop.niacc.edu>
2020-12-02 10:06:09 -0600

    Rock Paper Scissors Complete

--- RockPaperScissors/public_html/RockPaperScissors.js
+++ RockPaperScissors/public_html/RockPaperScissors.js
@@ -1,17 +1,59 @@
-//function generateComputerChoice() 
-var generateComputerChoice = Math.random() * 3;
-if (generateComputerChoice === 1) {
-    generateComputerChoice = "rock";
-    } else if (generateComputerChoice === 2) {
-        generateComputerChoice = "paper";
+function generateComputerChoice() {
+var computerChoice = Math.random() * 3;
+computerChoice = Math.floor(computerChoice) +1;
+if (computerChoice === 1) {
+    generateComputerChoice = "Rock";
+    } else if (computerChoice === 2) {
+        generateComputerChoice = "Paper";
     } else {
-        generateComputerChoice = "scisors";
-    }
+        generateComputerChoice = "Scissors";
+    } 
+    return generateComputerChoice;
+} 
+
 //generate a random number between 1 and 3
 //1 = rock, 2 = paper, 3 = scissors
 //the function should return the name of the computer's choice
 
 function PlayTheGame() {
+    var radioButtons = document.getElementsByName ("choice");
+    var index = 0;
+    while (index < radioButtons.length) {
+        if (radioButtons [index].checked === true ) {
+            rpsChoice = radioButtons[index].value;
+        }
+        ++index;
+    }
+    let cpuWeapon = generateComputerChoice();
+    alert ("Computer chose " + cpuWeapon);
+    if (rpsChoice === cpuWeapon) {
+        alert ("It's a tie!");
+    }
+    else if (rpsChoice === "Rock") {
+        if (cpuWeapon === "Scissors") {
+            alert ("You win, Rock breaks Scissors.");
+        } else {
+            alert ("You lose, Paper covers Rock.");
+        }
+    }
+    else if (rpsChoice === "Paper") {
+        if (cpuWeapon === "Rock") {
+            alert ("You win, Paper covers Rock.");
+            } else {
+                if (cpuWeapon === "Scissors") {
+                    alert ("You lose, Scissors cut Paper.");
+                }
+        }
+    }
+    else if (rpsChoice === "Scissors") {
+        if (cpuWeapon === "Rock") {
+            alert ("You lose, Rock breaks Scissors.");
+        } else {
+            if (cpuWeapon === "Paper") {
+                alert ("You win, Scissors cut Paper.");
+            }
+        }
+    }
 //declare variables to hold the player choice and the computer choice
 
 
